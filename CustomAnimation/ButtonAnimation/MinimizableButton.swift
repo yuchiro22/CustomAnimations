f9d38d28491d399d17a01744bea9b255f153e56d
yuchiro22 <t.yuyu.alies@gmail.com>
2020-03-01 09:17:30 +0900

    fix: resizeのタイミングをcancellとendに変更

--- CustomAnimation/ButtonAnimation/MinimizableButton.swift
+++ CustomAnimation/ButtonAnimation/MinimizableButton.swift
@@ -6,14 +6,12 @@ final class MinimizableButton: UIButton {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         UIView.animate(withDuration: 0.2, animations: {
             self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-        }, completion: { _ in
-            self.transform = CGAffineTransform.identity
         })
     }
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        self.transform = CGAffineTransform.identity
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        self.transform = CGAffineTransform.identity
     }
 }
