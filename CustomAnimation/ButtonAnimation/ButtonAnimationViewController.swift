fda5111d67e94dbe0255a2ac4e28e76edf3a856a
yuchiro22 <t.yuyu.alies@gmail.com>
2020-03-02 09:03:22 +0900

    add: SingleSonarButton

--- CustomAnimation/ButtonAnimation/ButtonAnimationViewController.swift
+++ CustomAnimation/ButtonAnimation/ButtonAnimationViewController.swift
@@ -3,10 +3,13 @@ import UIKit
 class ButtonAnimationViewController: UIViewController {
     @IBOutlet private weak var fadeButton: FadableButton!
     @IBOutlet private weak var changeBackgroundColorButton: ChangeBackgroundColorButton!
+    @IBOutlet private weak var singleSonarButton: SingleSonarButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         changeBackgroundColorButton.layer.borderWidth = 2.0
         changeBackgroundColorButton.layer.borderColor = UIColor.black.cgColor
+        singleSonarButton.layer.borderWidth = 2.0
+        singleSonarButton.layer.borderColor = UIColor.black.cgColor
     }
 }
